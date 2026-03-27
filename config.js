bbe900dd284e5adbf791939f64f66eb004f0c83e
m7mmaz <qiqx70@gmail.com>
2026-03-27 15:02:15 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  enableOnlineLookup: true,
+  apiBaseUrl: '/api/analyze',
+  dailySearchLimit: 25,
+  apiProvider: 'gemini'
+};
---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'food-sensitivity-smart-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './config.js',
+  './icon.svg',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+  './favicon-64.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
